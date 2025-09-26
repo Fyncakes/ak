2f51e8fb1caa6e035fbd7835713fad87ca77f90b
aron <kolarony8@gmail.com>
2025-09-26 19:13:17 +0300

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # main.py
-import os
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -10,5 +9,4 @@ app = create_app()
 if __name__ == '__main__':
     # For development, run with debug=True.
     # The production server (Gunicorn) will run this file without __main__.
-    port = int(os.environ.get('FLASK_RUN_PORT', 5001))
-    app.run(debug=True, port=port)
+    app.run(debug=True)
